5c8e3ab5012fb90ef28affbc71b2fa0356d7cf90
Mark Proctor <mdproctor@gmail.com>
2026-08-03 06:44:36 +0100

    feat(#103): CaseDefinition types, case adapter, stencils, read-only viewer
    Refs #103

--- packages/graph-stencil-case/src/index.ts
+++ packages/graph-stencil-case/src/index.ts
@@ -1,3 +1,17 @@
-export { CaseAdapter } from './adapter/case-adapter.js';
-export { caseStencils } from './stencils/index.js';
-export type { CaseDefinition } from './types/case-definition.js';
+export { toGraph } from './adapter/case-adapter.js';
+export { toReactFlowGraph } from './adapter/react-flow-transform.js';
+export type { RFNode, RFEdge } from './adapter/react-flow-transform.js';
+export { registerCaseStencils } from './stencils/index.js';
+export { renderBinding, renderWorker, renderMilestone, renderGoal, renderSubCase } from './stencils/index.js';
+export type {
+  CaseDefinition,
+  CaseDefinitionSpec,
+  Binding,
+  Worker,
+  Milestone,
+  Goal,
+  SubCase,
+  Capability,
+  HumanTask,
+  Trigger,
+} from './types/case-definition.js';
